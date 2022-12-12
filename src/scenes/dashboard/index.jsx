e1d571abb16a84169572a3cf0c80fa99646a1835
user name <98519572+OkolieOKS234@users.noreply.github.com>
2022-12-12 21:09:32 +0100

    added import for my dashboard section

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,18 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../theme";
+import { mockTransactions } from "../../data/mockData";
+import EmailIcon from "@mui/icons-material/DownloadOutlined";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import TrafficIcon from "@mui/icons-material/Traffic";
+
+import BarChart from "../../components/Barchart";
+import GeographyChart from "../../components/GeographyChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+  const theme = useTheme();
   return <div>Side</div>;
 };
 export default Dashboard;
